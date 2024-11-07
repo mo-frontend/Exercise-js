14a16aa855c13cb9857386ae13438bd603ee213b
hurre <jacaa47@gmail.com>
2024-11-07 17:42:47 +0700

    added as commant the result

--- All_Exercise/javaScirpt_Basics/Exercise#1/main.js
+++ All_Exercise/javaScirpt_Basics/Exercise#1/main.js
@@ -1,10 +1,10 @@
 let x = 7;
 let y = 3;
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
-console.log(x++);
-console.log(x--);
+console.log(x + y); // 10
+console.log(x - y); // 4
+console.log(x * y); // 21
+console.log(x / y); // 2.3333333333335
+console.log(x % y); // 1
+console.log(x++); // 7
+console.log(x--); // 8
