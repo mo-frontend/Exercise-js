471a92d599615c7ca145e12dcd55e8cc7ef0ca84
hurre <jacaa47@gmail.com>
2024-11-08 15:55:49 +0700

    added exercise #4

---
+++ All_Exercise/javaScirpt_Basics/Exercise#4/main.js
@@ -0,0 +1,20 @@
+// Basic function
+
+function add(nam1, nam2) {
+  return nam1 + nam2;
+}
+console.log(add(3, 4)); // 7
+console.log(add(6, 9)); // 15
+console.log(add(9, 4)); // 13
+console.log(add(2, 4)); // 6
+console.log(add(310, 4)); // 314
+
+// Experssions function
+
+const sum = function (nam1, nam2) {
+  return nam1 + nam2;
+};
+console.log(sum(5, 9)); // 14
+console.log(sum(511, 9)); // 520
+console.log(sum(5, 99)); //  104
+console.log(sum(7, 966)); // 973
