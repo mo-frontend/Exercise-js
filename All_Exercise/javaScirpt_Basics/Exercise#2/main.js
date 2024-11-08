c369023723c189f25724275a77b75ede6d6af260
hurre <jacaa47@gmail.com>
2024-11-08 14:09:37 +0700

    added exercise 2

---
+++ All_Exercise/javaScirpt_Basics/Exercise#2/main.js
@@ -0,0 +1,19 @@
+let a = 15;
+let b = "15";
+
+// Equal to
+console.log(a == b); // true
+// Not Equal to
+console.log(a != b); // false
+// Strictly Equal
+console.log(a === b); // false
+// Strictly Not Equal
+console.log(a !== b); // true
+// Greater than
+console.log(a > b); // false
+// less than
+console.log(a < b); // false
+// Greater than or Equal
+console.log(a >= b); // true
+// less than or Equal
+console.log(a <= b); // true
